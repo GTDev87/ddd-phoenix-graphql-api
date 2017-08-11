6a3043241bfd2375248760228e321aca8c01d4a9
Greg <gregt87@gmail.com>
2017-08-11 04:23:31 -0400

    added username field for proof of concept

--- app/domains/post.ex
+++ app/domains/post.ex
@@ -42,5 +42,17 @@ defmodule App.Domains.Post do
         end)
       end
     end
+    field :user_name, :string do
+      # how do i get the user_id here if all i have access to is the id?
+      # need to indicate dependencies
+      # dependency is post(user_id) here
+
+      resolve fn id, _, _ ->
+        # Logger.debug "body id = #{id}"
+        App.Lib.MultiBatch.batch([{&App.Models.Post.ids/1, id}, {&App.Models.User.ids/1, :user_id}], fn (batch_results) ->
+          {:ok, batch_results |> Map.get(id) |> Map.get(:name)}
+        end)
+      end
+    end
   end
 end
--- app/domains/user.ex
+++ app/domains/user.ex
@@ -6,15 +6,15 @@ defmodule App.Domains.User do
   object :user do
     field :id, :id do
       resolve fn id, _, _ ->
-        App.Lib.MultiBatch.batch([{&App.Models.Post.ids/1, id}], fn (batch_results) ->
+        App.Lib.MultiBatch.batch([{&App.Models.User.ids/1, id}], fn (batch_results) ->
           {:ok, batch_results |> Map.get(id) |> Map.get(:id)}
         end)
       end
     end
     field :name, :string do
       resolve fn id, _, _ ->
-        App.Lib.MultiBatch.batch([{&App.Models.Post.ids/1, id}], fn (batch_results) ->
-          {:ok, batch_results |> Map.get(id) |> Map.get(:title)}
+        App.Lib.MultiBatch.batch([{&App.Models.User.ids/1, id}], fn (batch_results) ->
+          {:ok, batch_results |> Map.get(id) |> Map.get(:name)}
         end)
       end
     end
