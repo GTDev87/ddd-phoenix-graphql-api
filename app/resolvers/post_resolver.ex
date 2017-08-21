88f202a4fabfd69300a5352e726787914495b618
Greg <gregt87@gmail.com>
2017-08-20 21:09:30 -0400

    mutations now return only id for filling out data

--- app/resolvers/post_resolver.ex
+++ app/resolvers/post_resolver.ex
@@ -5,33 +5,38 @@ defmodule App.PostResolver do
   def all(_args, _info) do
     {
       :ok,
-      App.Repo.all(App.Models.Post)
+      App.ReadWriteRepo.all(:query, App.Models.Post)
       |> Enum.map(fn p -> p.id end)
     }
   end
 
   def find(%{id: id}, _info) do
-    ids = App.Repo.get(App.Models.Post, id)
-    case ids do
+    App.ReadWriteRepo.get(:query, App.Models.Post, id)
+    |> case do
       nil -> {:error, "Post id #{id} not found"}
       post -> {:ok, post.id}
     end
   end
 
   def create(args, _info) do
-    %App.Models.Post{}
-    |> App.Models.Post.changeset(args)
-    |> App.Repo.insert
+    changeset =
+      %App.Models.Post{}
+      |> App.Models.Post.changeset(args)
+    {:ok, returned_post} = App.ReadWriteRepo.insert(:mutation, changeset)
+    {:ok, returned_post.id}
   end
 
   def update(%{id: id, post: post_params}, _info) do
-    App.Repo.get!(App.Models.Post, id)
-    |> App.Models.Post.changeset(post_params)
-    |> App.Repo.update
+    changeset =
+      App.Repo.get!(App.Models.Post, id)
+      |> App.Models.Post.changeset(post_params)
+    {:ok, returned_post} = App.Repo.update(:mutation, changeset)
+    {:ok, returned_post.id}
   end
 
   def delete(%{id: id}, _info) do
     post = App.Repo.get!(App.Models.Post, id)
-    App.Repo.delete(post)
+    {:ok, returned_post} = App.Repo.delete(:mutation, post)
+    {:ok, returned_post.id}
   end
 end
