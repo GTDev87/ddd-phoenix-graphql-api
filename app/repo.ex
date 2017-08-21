1f53e14228e4b7dae252b00dcb6d356f24c1424f
Greg <gregt87@gmail.com>
2017-08-21 01:17:32 -0400

    moved a portion of the repo special functions into own file

--- app/repo.ex
+++ app/repo.ex
@@ -8,20 +8,13 @@ defmodule App.WriteRepo do
   require Logger
 end
 
-defmodule App.RepoUtil do
-  def create_args(_, 0),
-    do: []
-  def create_args(fn_mdl, arg_cnt),
-    do: Enum.map(1..arg_cnt, &(Macro.var (:"arg#{&1}"), fn_mdl))
-end
-
 defmodule App.ReadWriteRepo do
   require Logger
 
   (App.Repo.__info__ :functions)
   |> Enum.each(fn {func_name, arity} ->
-    fn_args = App.RepoUtil.create_args(App.Repo, arity)
-    all_fn_args = [Macro.var(:type, nil)] ++ App.RepoUtil.create_args(App.Repo, arity)
+    fn_args = App.Lib.RepoUtils.create_args(App.Repo, arity)
+    all_fn_args = [Macro.var(:type, nil)] ++ App.Lib.RepoUtils.create_args(App.Repo, arity)
 
     def unquote(:"#{func_name}")(unquote_splicing(all_fn_args)) do
       case type do
