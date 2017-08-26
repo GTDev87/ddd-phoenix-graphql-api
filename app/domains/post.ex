dbe6921581d2e3ea302d27d8f694ffdba041c91f
Greg <gregt87@gmail.com>
2017-08-26 11:48:52 -0400

    removed some comments

--- app/domains/post.ex
+++ app/domains/post.ex
@@ -7,7 +7,6 @@ defmodule App.Domains.Post do
   # only expectation is id
   object :post do
     field :id, :id do
-      # dependency is post_id here
       resolve fn id, _, info ->
         App.Lib.MultiBatch.batch_dependency({&App.Models.Post.ids/2, id}, fn (batch_results) ->
           {:ok, batch_results |> Map.get(id) |> Map.get(:id)}
@@ -15,35 +14,21 @@ defmodule App.Domains.Post do
       end
     end
     field :title, :string do
-      # dependency is post_id here
       resolve fn id, _, info ->
-        type = App.Lib.Resolver.query_type(info)
-        # Logger.debug "post title type = #{type}"
-
         App.Lib.MultiBatch.batch_dependency({&App.Models.Post.ids/2, id}, fn (batch_results) ->
           {:ok, batch_results |> Map.get(id) |> Map.get(:title)}
         end, query_type: App.Lib.Resolver.query_type(info))
       end
     end
     field :body, :string do
-      # dependency is post_id here
       resolve fn id, _, info ->
-        type = App.Lib.Resolver.query_type(info)
-        # Logger.debug "post body type = #{type}"
-
         App.Lib.MultiBatch.batch_dependency({&App.Models.Post.ids/2, id}, fn (batch_results) ->
           {:ok, batch_results |> Map.get(id) |> Map.get(:body)}
         end, query_type: App.Lib.Resolver.query_type(info))
       end
     end
     field :user, :user do
-      # how do i get the user_id here if all i have access to is the id?
-      # need to indicate dependencies
-      # dependency is post(user_id) here
-
       resolve fn id, _, info ->
-        type = App.Lib.Resolver.query_type(info)
-        # Logger.debug "post user type = #{type}"
         App.Lib.MultiBatch.batch_serial_dependencies([{&App.Models.Post.ids/2, id}, {&App.Models.User.ids/2, :user_id}], fn (batch_results) ->
 
           {:ok, batch_results |> Map.get(id, %{}) |> Map.get(:id)}
@@ -51,15 +36,7 @@ defmodule App.Domains.Post do
       end
     end
     field :user_name, :string do
-      # how do i get the user_id here if all i have access to is the id?
-      # need to indicate dependencies
-      # dependency is post(user_id) here
-
-
-
       resolve fn id, _, info ->
-        type = App.Lib.Resolver.query_type(info)
-        # Logger.debug "post user_name type = #{type}"
         App.Lib.MultiBatch.batch_serial_dependencies([{&App.Models.Post.ids/2, id}, {&App.Models.User.ids/2, :user_id}], fn (batch_results) ->
           {:ok, batch_results |> Map.get(id) |> Map.get(:name)}
         end, query_type: App.Lib.Resolver.query_type(info))
