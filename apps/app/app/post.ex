a97c68be1d268ba9abb29b03ae8d886c91052d3d
Greg <gregt87@gmail.com>
2017-08-27 00:41:18 -0400

    removed extra text

--- apps/app/app/post.ex
+++ apps/app/app/post.ex
@@ -46,8 +46,6 @@ defmodule App.Post do
 
   # Operations
   def all(_args, info) do
-    type = App.Lib.Resolver.query_type(info)
-    # Logger.debug "all type = #{type}"
     {
       :ok,
       App.ReadWriteRepo.all(:query, App.Post.Post)
@@ -56,9 +54,6 @@ defmodule App.Post do
   end
 
   def find(%{id: id}, info) do
-    type = App.Lib.Resolver.query_type(info)
-    Logger.debug "find type = #{type}"
-    # Logger.debug "_info.parent_type.name = #{inspect _info.parent_type.name}"
     App.ReadWriteRepo.get(:query, App.Post.Post, id)
     |> case do
       nil -> {:error, "Post id #{id} not found"}
@@ -67,9 +62,6 @@ defmodule App.Post do
   end
 
   def create(args, info) do
-    type = App.Lib.Resolver.query_type(info)
-    Logger.debug "create type = #{type}"
-    
     changeset =
       %App.Post.Post{}
       |> App.Post.Post.changeset(args)
@@ -78,9 +70,6 @@ defmodule App.Post do
   end
 
   def update(%{id: id, post: post_params}, info) do
-    type = App.Lib.Resolver.query_type(info)
-    Logger.debug "update type = #{type}"
-
     changeset =
       App.Repo.get!(App.Post.Post, id)
       |> App.Post.Post.changeset(post_params)
@@ -89,9 +78,6 @@ defmodule App.Post do
   end
 
   def delete(%{id: id}, info) do
-    type = App.Lib.Resolver.query_type(info)
-    Logger.debug "delete type = #{type}"
-
     post = App.Repo.get!(App.Post.Post, id)
     {:ok, returned_post} = App.Repo.delete(:mutation, post)
     {:ok, returned_post.id}
